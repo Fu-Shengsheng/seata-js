eef2045d6a94f53639d36a2248e94ed9dc561e50
root <root@DESKTOP-P8S2AHT.localdomain>
2022-09-20 00:20:29 +0800

    feat: zookeeper-configuration base struct

---
+++ packages/seata-js/src/seata-common/configuration-keys.ts
@@ -0,0 +1,19 @@
+/**
+ * The type Configuration keys.
+ *
+ * @author Fss
+ */
+export enum ConfigurationKeys {
+    /**
+     * The constant SEATA_FILE_ROOT_CONFIG
+     */
+    SEATA_FILE_ROOT_CONFIG = 'seata',
+    /**
+     * The constant FILE_ROOT_CONFIG.
+     */
+    FILE_ROOT_CONFIG = 'config',
+    /**
+     * The constant FILE_CONFIG_SPLIT_CHAR.
+     */
+    FILE_CONFIG_SPLIT_CHAR = '.',
+}
---
+++ packages/seata-js/src/seata-config/config-zk/zookeeper-configuration.ts
@@ -0,0 +1,39 @@
+import Zookeeper from 'zookeeper'
+import { AbstractConfiguration } from "../config-core/abstract-configuration"
+import { ConfigurationKeys } from '../../seata-common/configuration-keys'
+
+/**
+ * The type Zookeeper configuration
+ *
+ * @class ZookeeperConfiguration
+ * @extends {AbstractConfiguration}
+ * 
+ * @author Fss
+ */
+class ZookeeperConfiguration extends AbstractConfiguration {
+    private static CONFIG_TYPE = 'zk'
+    private static ZK_PATH_SPLIT_CHAR = '/'
+    private static ROOT_PATH = ZookeeperConfiguration.ZK_PATH_SPLIT_CHAR + ConfigurationKeys.SEATA_FILE_ROOT_CONFIG
+    // TO-DO: 等待补全 ConfigurationFactory 的定义
+    // private static FILE_CONFIG: Configuration = ConfigurationFactory.CURRENT_FILE_INSTANCE;
+    private static SERVER_ADDR_KEY = "serverAddr";
+    private static SESSION_TIMEOUT_KEY = "sessionTimeout";
+    private static CONNECT_TIMEOUT_KEY = "connectTimeout";
+    private static AUTH_USERNAME = "username";
+    private static AUTH_PASSWORD = "password";
+    private static SERIALIZER_KEY = "serializer";
+    private static CONFIG_PATH_KEY = "nodePath";
+    private static THREAD_POOL_NUM = 1;
+    private static DEFAULT_SESSION_TIMEOUT = 6000;
+    private static DEFAULT_CONNECT_TIMEOUT = 2000;
+    private static DEFAULT_CONFIG_PATH = ZookeeperConfiguration.ROOT_PATH + "/seata.properties";
+    private static FILE_CONFIG_KEY_PREFIX = ConfigurationKeys.FILE_ROOT_CONFIG + ConfigurationKeys.FILE_CONFIG_SPLIT_CHAR + ZookeeperConfiguration.CONFIG_TYPE + ConfigurationKeys.FILE_CONFIG_SPLIT_CHAR
+    // TO-DO: java.util.concurrent 底层依赖库的 TS 对应实现
+    // private static CONFIG_EXECUTOR:ExecutorService = new ThreadPoolExecutor(THREAD_POOL_NUM, THREAD_POOL_NUM, Integer.MAX_VALUE, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), new NamedThreadFactory("ZKConfigThread", THREAD_POOL_NUM))
+    private static zkClient: Zookeeper
+    private static MAP_INITIAL_CAPACITY = 8;
+    // TO-DO: java.util.concurrent 底层依赖库的 TS 对应实现
+    // private static ConcurrentMap<String, ConcurrentMap<ConfigurationChangeListener, ZKListener>> CONFIG_LISTENERS_MAP = new ConcurrentHashMap<>(MAP_INITIAL_CAPACITY)
+    // TO-DO: java.util 底层依赖库的 TS 对应实现
+    // private static Properties seataConfig = new Properties();
+}
